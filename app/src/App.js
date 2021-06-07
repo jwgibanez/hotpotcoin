7b6c3ec08263d56d1e2f36e9ca0366422c62eabf
metahashxyz <81914330+metahashxyz@users.noreply.github.com>
2021-06-08 00:06:16 +0800

    Updated app

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import detectEthereumProvider from '@metamask/detect-provider';
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [dispAccount, setDispAccount] = useState('Connect');
   const init = async () => {
     const provider = await detectEthereumProvider();
     if (provider) {
-      console.log('Ethereum successfully detected!')
+      console.log('Ethereum successfully detected!');
       const element = 'Metamask detected.';
-      ReactDOM.render(element, document.getElementById('message'));
+      let acc = provider.selectedAddress;
+      if (acc) {
+        setAccount(acc);
+        setDispAccount(acc.substring(0, 5) + '...' + acc.substring(acc.length-3, acc.length));
+        //ReactDOM.render(element, document.getElementById('message'));
+      }
     } else {
       console.log('Please install MetaMask!');
       const element = 'Please install MetaMask.';
-      ReactDOM.render(element, document.getElementById('message'));
+      //ReactDOM.render(element, document.getElementById('message'));
     }
   };
   init();
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p id="message">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">HOTPOT COIN</a>
+          <button type="button"
+            className={account ? 'btn btn-success' : 'btn btn-warning'}>{dispAccount}</button>
+        </div>
+      </nav>
+      <div className={ `alert' + ${ account ? 'alert alert-success' : 'alert alert-info' }`} role="alert">
+        { account ? 'Connection established.' : 'Waiting for connection...' }
+      </div>
+    </div>);
 }
 
 export default App;
