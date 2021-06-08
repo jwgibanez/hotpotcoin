cf41e522fae37e2c03c68fbc66e5586f50b18a6a
metahashxyz <81914330+metahashxyz@users.noreply.github.com>
2021-06-09 00:03:39 +0800

    App: show airdrop balance

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import detectEthereumProvider from '@metamask/detect-provider';
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import Web3 from 'web3';
+import abi from './Airdrop.json';
 
 function App() {
   const [account, setAccount] = useState(null);
   const [dispAccount, setDispAccount] = useState('Connect');
-  const init = async () => {
-    const provider = await detectEthereumProvider();
-    if (provider) {
-      console.log('Ethereum successfully detected!');
-      const element = 'Metamask detected.';
-      let acc = provider.selectedAddress;
-      if (acc) {
+  const [airdropBalance, setAirdropBalance] = useState(0);
+  let web3, airdrop;
+  const walletConnect = async() => {
+    console.log('Checking wallet connect...');
+
+    // Create a connector
+    const connector = new WalletConnect({
+      bridge: "https://bridge.walletconnect.org", // Required
+      qrcodeModal: QRCodeModal,
+    });
+
+    // Check if connection is already established
+    if (!connector.connected) {
+      // create new session
+      connector.createSession();
+    }
+
+    // Subscribe to connection events
+    connector.on("connect", (error, payload) => {
+      if (error) {
+        //throw error;
+        console.log('No MetaMask or WalletConnect found.');
+        const element = 'Please install MetaMask.';
+        return;
+      }
+
+      // Get provided accounts and chainId
+      const { accounts, chainId } = payload.params[0];
+
+      if (accounts) {
+        let acc = accounts[0];
         setAccount(acc);
         setDispAccount(acc.substring(0, 5) + '...' + acc.substring(acc.length-3, acc.length));
-        //ReactDOM.render(element, document.getElementById('message'));
       }
+    });
+
+    connector.on("session_update", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      // Get updated accounts and chainId
+      const { accounts, chainId } = payload.params[0];
+    });
+
+    connector.on("disconnect", (error, payload) => {
+      if (error) {
+        throw error;
+      }
+
+      // Delete connector
+    });
+  };
+
+  const init = async () => {
+    const provider = await detectEthereumProvider();
+    web3 = new Web3(provider);
+    airdrop = new web3.eth.Contract(abi,'0xc1c412260c017c7da3dD3499e562447553f0a9b2');
+    if (provider) {
+      provider
+        .request({ method: 'eth_accounts' })
+        .then((accounts) => {
+          let acc = accounts[0];
+          console.log(acc);
+          if (acc) {
+            setAccount(acc);
+            setDispAccount(acc.substring(0, 5) + '...' + acc.substring(acc.length-3, acc.length));
+            airdrop.methods.getBalance().call()
+            .then((balance) => {
+              setAirdropBalance(balance);
+            })
+          } else {
+            //walletConnect();
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     } else {
-      console.log('Please install MetaMask!');
-      const element = 'Please install MetaMask.';
-      //ReactDOM.render(element, document.getElementById('message'));
+      //walletConnect();
     }
   };
-  init();
+
+  $(function() {
+    init();
+  });
 
   return (
     <div className="App">
@@ -37,6 +110,9 @@ function App() {
       </nav>
       <div className={ `alert' + ${ account ? 'alert alert-success' : 'alert alert-info' }`} role="alert">
         { account ? 'Connection established.' : 'Waiting for connection...' }
+      </div>
+      <div className="alert alert-info" role="alert">
+        Remaining airdrop balance is<br/>{airdropBalance ? Web3.utils.fromWei(airdropBalance) + ' HOTPOT': '?'}
       </div>
     </div>);
 }
