fbbb84c2c32fd45b76df0f1644be95be72649ef8
metahashxyz <81914330+metahashxyz@users.noreply.github.com>
2021-06-10 15:24:09 +0800

    Updated app

--- app/src/App.js
+++ app/src/App.js
@@ -7,13 +7,15 @@ import detectEthereumProvider from '@metamask/detect-provider';
 import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "@walletconnect/qrcode-modal";
 import Web3 from 'web3';
-import abi from './Airdrop.json';
+import hotpotAbi from './Hotpot.json';
+import airdropAbi from './Airdrop.json';
 
 function App() {
   const [account, setAccount] = useState(null);
   const [dispAccount, setDispAccount] = useState('Connect');
   const [airdropBalance, setAirdropBalance] = useState(0);
-  let web3, airdrop;
+  const [walletBalance, setWalletBalance] = useState('0');
+  let web3, token, airdrop;
   const walletConnect = async() => {
     console.log('Checking wallet connect...');
 
@@ -69,35 +71,87 @@ function App() {
   const init = async () => {
     const provider = await detectEthereumProvider();
     web3 = new Web3(provider);
-    airdrop = new web3.eth.Contract(abi,'0xc1c412260c017c7da3dD3499e562447553f0a9b2');
+    token = new web3.eth.Contract(hotpotAbi,'0xD67b8B287aBDcb1F65b1477B5Fb3723a79710cE5');
+    airdrop = new web3.eth.Contract(airdropAbi,'0xc1c412260c017c7da3dD3499e562447553f0a9b2');
     if (provider) {
       provider
         .request({ method: 'eth_accounts' })
         .then((accounts) => {
           let acc = accounts[0];
-          console.log(acc);
           if (acc) {
             setAccount(acc);
             setDispAccount(acc.substring(0, 5) + '...' + acc.substring(acc.length-3, acc.length));
+            // Get airdop balance
             airdrop.methods.getBalance().call()
             .then((balance) => {
               setAirdropBalance(balance);
-            })
+              renderAirdropInfo();
+            });
+            // Get amount held by accounts
+            token.methods.balanceOf(acc).call()
+            .then((balance) => {
+              setWalletBalance(balance);
+              renderWalletBalance();
+              renderClaimButton();
+            });
           } else {
             //walletConnect();
           }
+          renderConnectionInfo();
         })
         .catch((err) => {
           console.error(err);
         });
     } else {
-      //walletConnect();
+      renderConnectionInfo();
     }
   };
 
   $(function() {
     init();
   });
+
+  function renderConnectionInfo() {
+    ReactDOM.render(
+      <div className={ `alert' + ${ account ? 'alert alert-success' : 'alert alert-secondary' }`} role="alert">
+        { account ? 'Connection established.' : 'No connection established.' }
+      </div>,
+      document.getElementById('connection-info')
+    );
+  }
+
+  function renderAirdropInfo() {
+    ReactDOM.render(
+      <div className="alert alert-info" role="alert">
+        Remaining airdrop balance is<br/>{airdropBalance ? Web3.utils.fromWei(airdropBalance).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' HOTPOT.': '...'}
+      </div>,
+      document.getElementById('airdrop-info')
+    );
+  }
+
+  function renderWalletBalance() {
+    ReactDOM.render(
+      <div className="alert alert-info" role="alert">
+        Your wallet currently holds<br/>{Web3.utils.fromWei(walletBalance).replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' HOTPOT.'}
+      </div>,
+      document.getElementById('wallet-info')
+    );
+  }
+
+  function renderClaimButton() {
+    ReactDOM.render(
+      <div className="d-grid gap-2">
+        <button type="button" className="btn btn-primary btn-lg" onClick={claim}>
+          Claim 1,000,000 HOTPOT
+        </button>
+      </div>,
+      document.getElementById('claim-info')
+    );
+  }
+
+  function claim() {
+    alert('TODO');
+  }
 
   return (
     <div className="App">
@@ -108,12 +162,14 @@ function App() {
             className={account ? 'btn btn-success' : 'btn btn-warning'}>{dispAccount}</button>
         </div>
       </nav>
-      <div className={ `alert' + ${ account ? 'alert alert-success' : 'alert alert-info' }`} role="alert">
-        { account ? 'Connection established.' : 'Waiting for connection...' }
+      <div id='connection-info'>
+        <div className="alert alert-secondary" role="alert">
+          Trying to establish connection...
+        </div>
       </div>
-      <div className="alert alert-info" role="alert">
-        Remaining airdrop balance is<br/>{airdropBalance ? Web3.utils.fromWei(airdropBalance) + ' HOTPOT': '?'}
-      </div>
+      <div id='airdrop-info'/>
+      <div id='wallet-info'/>
+      <div id='claim-info'/>
     </div>);
 }
 
